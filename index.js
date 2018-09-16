647634b86822408c929f4c307e504381546550c4
Matheus Luís <suetham_luis@hotmail.com>
2018-09-16 16:04:52 -0300

    Database connection

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const app = require('./lib/application');
-const Logger = require('./lib/config/winston');
+const logger = require('./lib/config/winston');
+const database = require('./lib/config/db');
+const keys = require('./lib/config/key');
 
 const PORT = process.env.PORT || 5000;
 
+database.connect(keys.mongoURI, (err) => {
+    if (err) {
+        logger.error('Shutdown the application because an error occurred ' +
+            'when connecting to database');
+        process.exit(1);
+    }
+});
+
+const shutdown = () => {
+    logger.warn('Server receive signal to shutdown.');
+    process.exit(0);
+};
+
+process
+    .on('SIGTERM', shutdown)
+    .on('SIGINT', shutdown)
+    .on('SIGHUP', shutdown)
+    .on('uncaughtException', (er) => {
+        logger.error(`uncaughtException caught the error: ${er.message}`);
+        throw er;
+    })
+    .on('exit', (code) => {
+        logger.info('Node process exit with code:', code);
+        database.close();
+    });
+
 const server = app.listen(PORT, (err) => {
     if (err) {
-        Logger.error('Error on listen port. ', err.message);
+        logger.error('Error on listen port. ', err.message);
     }
-    Logger.info('Server starting at %s:%s.',
+    logger.info('Server starting at %s:%s.',
         server.address().address, server.address().port);
 
     server.on('close', () => {
-        Logger.info('Shutdown the application server');
+        logger.info('Shutdown the application server');
     });
 });
 
